2c1c5df06e4d40ef058bb0a01a03efd4f66d9b41
Boris Ulicny <ulicny.boris@gmail.com>
2019-05-02 14:36:07 +0200

    Remove blocking network reachability calls

--- UAAppReviewManager.m
+++ UAAppReviewManager.m
@@ -726,7 +726,7 @@ static NSString * const reviewURLTemplate                   = @"macappstore://it
 }
 
 - (void)showPromptIfNecessary:(BOOL)canPromptForRating {
-	if (canPromptForRating && [self ratingConditionsHaveBeenMet] && [self connectedToNetwork]) {
+	if (canPromptForRating && [self ratingConditionsHaveBeenMet]) {
 
 		__block BOOL shouldPrompt = YES;
 		if (self.shouldPromptBlock) {
@@ -767,7 +767,7 @@ static NSString * const reviewURLTemplate                   = @"macappstore://it
 }
 
 - (void)showPrompt {
-    if ((self.appID && [self connectedToNetwork] && ![self userHasDeclinedToRate] && ![self userHasRatedCurrentVersion]) || self.debugEnabled) {
+    if ((self.appID && ![self userHasDeclinedToRate] && ![self userHasRatedCurrentVersion]) || self.debugEnabled) {
         [self showRatingAlert];
     }
 }
@@ -1177,41 +1177,6 @@ static NSString * const reviewURLTemplate                   = @"macappstore://it
 	[self.userDefaultsObject setObject:@YES forKey:appiraterAlreadyCompletedKey];
 	[self.userDefaultsObject synchronize];
 }
-
-
-#pragma mark - Internet Connectivity
-
-- (BOOL)connectedToNetwork {
-    // Create zero addy
-    struct sockaddr_in zeroAddress;
-    bzero(&zeroAddress, sizeof(zeroAddress));
-    zeroAddress.sin_len = sizeof(zeroAddress);
-    zeroAddress.sin_family = AF_INET;
-
-    // Recover reachability flags
-    SCNetworkReachabilityRef defaultRouteReachability = SCNetworkReachabilityCreateWithAddress(NULL, (struct sockaddr *)&zeroAddress);
-    SCNetworkReachabilityFlags flags;
-
-    BOOL didRetrieveFlags = SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags);
-    CFRelease(defaultRouteReachability);
-
-    if (!didRetrieveFlags) {
-        UAAppReviewManagerDebugLog(@"Error. Could not recover network reachability flags");
-        return NO;
-    }
-
-    BOOL isReachable = flags & kSCNetworkFlagsReachable;
-    BOOL needsConnection = flags & kSCNetworkFlagsConnectionRequired;
-	BOOL nonWiFi = flags & kSCNetworkReachabilityFlagsTransientConnection;
-
-	NSURL *testURL = [NSURL URLWithString:@"https://www.apple.com/"];
-	NSURLRequest *testRequest = [NSURLRequest requestWithURL:testURL  cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:20.0];
-	// TODO: NSURLConnection deprecated
-	NSURLConnection *testConnection = [[NSURLConnection alloc] initWithRequest:testRequest delegate:self];
-
-	return ((isReachable && !needsConnection) || nonWiFi) && testConnection != nil;
-}
-
 
 #pragma mark - PRIVATE Misc Helpers
 
